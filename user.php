136b821a280761c8d257615c9a23cc37491930f4
juju <julien.adami@etu.iut-tlse3.fr>
2022-03-14 22:33:44 +0100

    launch

---
+++ user.php
@@ -0,0 +1,31 @@
+<?php
+include 'connexion.php';
+$userId =1;
+$host = 'eu-cdbr-west-02.cleardb.net';
+$dbname = 'heroku_495fd814c1f433b';
+$username = 'b56a58b253f64f';
+$password = '37327fda';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    echo "Connecté à $dbname sur $host avec succès.";
+
+} catch (PDOException $e) {
+
+    die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage());
+
+}
+function throwUser($userId){
+    $host = 'eu-cdbr-west-02.cleardb.net';
+    $dbname = 'heroku_495fd814c1f433b';
+    $username = 'b56a58b253f64f';
+    $password = '37327fda';
+    $sql = 'SELECT idUser, nom, prenom, montantBK FROM user WHERE idUser='+".$userId.";'';
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $response = $conn->query($sql);
+    $output = $response->fetch();
+    echo $output;
+    echo(json_encode($output));
+}
+
