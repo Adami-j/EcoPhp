f3ed61df99548bccc9ca65c3be06900c4d68b4de
juju <julien.adami@etu.iut-tlse3.fr>
2022-03-14 21:31:07 +0100

    launch

--- index.php
+++ index.php
@@ -9,23 +9,9 @@
 </head>
 <body>
 bonjour à tous tétatousse
-<?php echo "cococ";?>
-<?php
-$host = 'eu-cdbr-west-02.cleardb.net';
-$dbname = 'heroku_495fd814c1f433b';
-$username = 'b56a58b253f64f';
-$password = '37327fda';
+<?php echo "cococ";
+require 'connexion.php';?>
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-    echo "Connecté à $dbname sur $host avec succès.";
-
-} catch (PDOException $e) {
-
-    die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage());
-
-}?>
 
 </body>
 </html>
