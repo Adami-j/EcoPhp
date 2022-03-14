1b661643ee076494933b3b09088cb821263edbd1
juju <julien.adami@etu.iut-tlse3.fr>
2022-03-14 22:41:40 +0100

    launch

--- user.php
+++ user.php
@@ -2,10 +2,6 @@
 include 'connexion.php';
 $userId =1;
 
-
-
-
-
     $host = 'eu-cdbr-west-02.cleardb.net';
     $dbname = 'heroku_495fd814c1f433b';
     $username = 'b56a58b253f64f';
@@ -13,8 +9,8 @@ $userId =1;
     $sql = 'SELECT idUser, nom, prenom, montantBK FROM user WHERE idUser='+".$userId.";'';
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $response = $conn->query($sql);
-    $output = $response->fetch();
-    echo $output;
+    $output = $response->fetchAll();
+    echo (json_encode($output));
 
 ?>
 
