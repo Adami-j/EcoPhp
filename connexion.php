da7d2f619e5e7b5e0ba8b6074c56d42926141bd5
juju <julien.adami@etu.iut-tlse3.fr>
2022-03-14 21:07:01 +0100

    launch

--- connexion.php
+++ connexion.php
@@ -4,9 +4,11 @@ try {
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
     $bdd = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_495fd814c1f433b', 'b56a58b253f64f', '37327fda', $pdo_options);
     // Execute SQL request on the database.
-    $sql = 'SELECT id, name, type, price FROM product;';
+    $sql = 'SELECT idUser, nom, prenom, montantBk FROM user;';
     $response = $bdd->query($sql);
+
     $output = $response->fetchAll(PDO::FETCH_ASSOC);
+    echo $output;
     } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
