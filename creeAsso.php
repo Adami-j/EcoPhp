24b97d2a37fd3f6df2268a05b12358a748023cb4
juju <julien.adami@etu.iut-tlse3.fr>
2022-03-16 19:11:33 +0100

    launch

--- creeAsso.php
+++ creeAsso.php
@@ -2,6 +2,21 @@
 include "connexion.php";
 
 header("Content-Type:application/json");
+$host = 'eu-cdbr-west-02.cleardb.net';
+$dbname = 'heroku_495fd814c1f433b';
+$username = 'b56a58b253f64f';
+$password = '37327fda';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+} catch (PDOException $e) {
+
+    die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage());
+
+}
+
+
 
 $http_method = $_SERVER['REQUEST_METHOD'];
 switch ($http_method) {
