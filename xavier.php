92880573c5395593e3a529ae797625de7eb50f2c
juju <julien.adami@etu.iut-tlse3.fr>
2022-03-17 14:25:40 +0100

    launch

--- xavier.php
+++ xavier.php
@@ -4,7 +4,7 @@
 $server = "localhost" ;
 $login = "root";
 $mdp = "root";
-$db = "contact";
+$db = "xavier";
 ///Connexion au serveur MySQL
 try {
     $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
